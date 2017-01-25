c9d405248d11588c9981f7901040ad06ddf80bb6
voidmain <yassine.khachlek@gmail.com>
2017-01-25 21:56:28 +0100

    here we go

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# laravel-benchmark
+# Laravel Benchmark
+
+Provide benchmark for artisan command.
+
+### Installation
+
+Install wia composer:
+
+```
+composer require yk/laravel-benchmark
+```
+
+And add the service provider in config/app.php:
+
+```php
+Yk\LaravelBenchmark\LaravelBenchmarkProvider::class,
+```
+
+### How to use
+
+To calculate benchmark speeds for an artisan command:
+
+```
+php artisan benchmark command_name
+```
+
+You can easy save test results by redirecting the output to file, example:
+
+```
+php artisan benchmark command_name >> test.txt
+```
+
+## Note
+
+You can perform benchmarks only for commands without parameters
+
+## TODO
+
+1. Add support for commands with parameters
+2. Change performance values calculation by providing the average of multiple test.
+
+## License
+
+### GPLv2
+
+Copyright (c) 2016 Yassine Khachlek <yassine.khachlek@gmail.com>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
--- src/App/Console/Commands/Benchmark.php
+++ src/App/Console/Commands/Benchmark.php
@@ -4,6 +4,8 @@ namespace Yk\LaravelBenchmark\App\Console\Commands;
 
 use Illuminate\Console\Command;
 
+use Artisan;
+
 class Benchmark extends Command
 {
     /**
@@ -11,7 +13,7 @@ class Benchmark extends Command
      *
      * @var string
      */
-    protected $signature = 'benchmark';
+    protected $signature = 'benchmark {artisan_command}';
 
     /**
      * The console command description.
@@ -19,6 +21,8 @@ class Benchmark extends Command
      * @var string
      */
     protected $description = 'Command description';
+
+    public $report = [];
 
     /**
      * Create a new command instance.
@@ -37,6 +41,36 @@ class Benchmark extends Command
      */
     public function handle()
     {
-    	$this->info('OK');
+    	$command = $this->argument('artisan_command');
+
+        $this->report['COMMAND_NAME'] = $command;
+        $this->report['PHP_VERSION'] = phpversion();
+        $this->report['PLATFORM']   = php_uname('s').' '.php_uname('m').' '.php_uname('r');
+
+        $this->report['START'] = microtime(true);
+        
+        $memory_get_usage = memory_get_usage();
+
+        $exitCode = Artisan::call($command, [
+            
+        ]);
+
+        $this->report['END'] = microtime(true);
+
+        $this->report['TIME'] = ceil(($this->report['END'] - $this->report['START']) * pow(10, 2)) / pow(10, 2)." Seconds";
+
+        $this->report['MEMORY_PEAK_USAGE'] = round((memory_get_peak_usage() - $memory_get_usage)/pow(10, 6), 2, PHP_ROUND_HALF_UP).' Megabytes';
+
+        $this->report['START']  = date('Y-m-d H:i:s', $this->report['START']);
+        $this->report['END'] = date('Y-m-d H:i:s', $this->report['END']);
+        $this->report['OUTPUT_LENGTH'] = strlen(Artisan::output());
+
+        foreach (array_keys($this->report) as $key) {
+            $title = $key;
+            while (strlen($title)<20) {
+                $title .= " ";
+            }
+            $this->info($title.': '.$this->report[$key]);
+        }
     }
 }
